fe7bc600d4160d0a417020a26f708fcd9a0051b1
David Jovicic <codandepcina7@gmail.com>
2020-04-10 18:46:40 +0200

    I hate GitHub!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-<<<<<<< HEAD
 
 export class AppComponent {
   title = 'Angular web2-project';
 }
-
-=======
-export class AppComponent {
-  title = 'first-test-angular';
-}
->>>>>>> fb25993ae5839324596ede37be9658ef11a01e91
