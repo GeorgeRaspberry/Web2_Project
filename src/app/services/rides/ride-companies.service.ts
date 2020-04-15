99505c4188f796d96c1f95168c13efe4342f380a
David Jovicic <codandepcina7@gmail.com>
2020-04-15 12:00:42 +0200

    RideCompany photos looking good

--- src/app/services/rides/ride-companies.service.ts
+++ src/app/services/rides/ride-companies.service.ts
@@ -16,11 +16,11 @@ export class RideCompaniesService {
   mockedCompanies(): Array<RideCompany> {
     let allRideCompanies = new Array<RideCompany>();
 
-    const r1 = new RideCompany(1,"assets/rentcar1.jpg", 'Rentorius Carius', "Addis Ababa	, Ethiopia" , 'Drive like a real man!', 4.3);
-    const r2 = new RideCompany(2,"assets/rentcar2.jpg", 'KarzEkspress', 'Paris, France', 'We grant what you wish for!', 4.4);
-    const r3 = new RideCompany(3,"assets/rentcar3.jpg", 'ForCar', 'Novi Sad, Serbia', 'Bolje nemojte sa nama...', 0.2);
-    const r4 = new RideCompany(4,"assets/rentcar4.jpg", 'Smart Car', 'Vienna, Austria', 'Go fast or go home!', 3.6);
-    const r5 = new RideCompany(5,"assets/rentcar5.jpg", 'Cole Rental', "California, USA" , 'We are good!', 5.0);
+    const r1 = new RideCompany(1,"assets/rentcar1.png", 'Rentorius Carius', "Addis Ababa	, Ethiopia" , 'Drive like a real man!', 4.3);
+    const r2 = new RideCompany(2,"assets/rentcar2.png", 'KarzEkspress', 'Paris, France', 'We grant what you wish for!', 4.4);
+    const r3 = new RideCompany(3,"assets/rentcar3.png", 'ForCar', 'Novi Sad, Serbia', 'We all for ti!', 0.2);
+    const r4 = new RideCompany(4,"assets/rentcar4.png", 'Smart Car', 'Vienna, Austria', 'Go fast or go home!', 3.6);
+    const r5 = new RideCompany(5,"assets/rentcar5.png", 'Cole Rental', "California, USA" , 'We are good!', 5.0);
 
     allRideCompanies.push(r1);
     allRideCompanies.push(r2);
@@ -28,7 +28,6 @@ export class RideCompaniesService {
     allRideCompanies.push(r4);
     allRideCompanies.push(r5);
     
-
     return allRideCompanies;
   }
 }
