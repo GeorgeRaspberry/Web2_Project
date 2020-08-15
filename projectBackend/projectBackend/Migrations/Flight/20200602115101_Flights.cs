718123fa023ea5f2cd18d3d021b8d0b13b4f0648
Duka <Duka@DESKTOP-R25HQR1>
2020-08-15 16:46:58 +0200

    data update

---
+++ projectBackend/projectBackend/Migrations/Flight/20200602115101_Flights.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace projectBackend.Migrations.Flight
+{
+    public partial class Flights : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FlightCompanies",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Image = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true),
+                    PromoDescription = table.Column<string>(nullable: true),
+                    Rating = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FlightCompanies", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Lastname = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Username = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    Role = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Flights",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FlyOffTime = table.Column<DateTime>(nullable: false),
+                    LandingTime = table.Column<DateTime>(nullable: false),
+                    FullFlightTime = table.Column<string>(nullable: true),
+                    FlightLength = table.Column<int>(nullable: false),
+                    NumberOfTransfers = table.Column<int>(nullable: false),
+                    LocationTransfers = table.Column<string>(nullable: true),
+                    Price = table.Column<int>(nullable: false),
+                    CompanyID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Flights", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Flights_FlightCompanies_CompanyID",
+                        column: x => x.CompanyID,
+                        principalTable: "FlightCompanies",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flights_CompanyID",
+                table: "Flights",
+                column: "CompanyID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Flights");
+
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.DropTable(
+                name: "FlightCompanies");
+        }
+    }
+}
